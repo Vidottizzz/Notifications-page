1f58976b0558d5fa66628e4664f270a6439ce7be
Vidotti <vidottizzz@gmail.com>
2023-02-25 15:38:09 -0300

    ENDED

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
-// notiDiv.addEventListener("click", myFunc() {
-//   // redDot.classList.add("hide");
-//  alert('Deu certo');
-// });
-
 let redDot = [...document.querySelectorAll(".red-dot")];
 let notiDiv = [...document.querySelectorAll(".noti_div")];
-let titleNum = document.querySelector("#tile-num");
+let titleNum = document.querySelector("#title-num");
 let allRead = document.querySelector("#right-top-text");
+let toggleMsg = document.querySelector("#hold_msg");
+let notiDiv1 = document.querySelector(".noti_div1");
 
 //fundo branco
+let count = 3;
+titleNum.innerHTML = count;
+
+function myFunction() {
+  if (toggleMsg.style.display === "none") {
+    toggleMsg.style.display = "block";
+  } else {
+    toggleMsg.style.display = "none";
+  }
+}
 
 notiDiv.map((el) => {
   el.addEventListener("click", () => {
     el.classList.add("background-read");
+    el.lastElementChild.firstElementChild.lastElementChild.classList.remove("red-dot");
+  
+      if (count > -1) {
+        count--;
+        titleNum.innerHTML = count;
+        if (count === 0) titleNum.classList.add("hide");
+      }
+    
   });
 
   // Ponto vermelho
-  redDot.map((elee) => {
-    elee.addEventListener("click", () => {
-      elee.classList.add("hide");
-    });
-  });
+  // redDot.map((el) => {
+  //   el.addEventListener("click", () => {
+  //     el.classList.remove("red-dot");
+  //   });
+  // });
 
-  // All
   redDot.map((ele) => {
     allRead.addEventListener("click", () => {
-      ele.classList.add("hide");
+      ele.classList.remove("red-dot");
       el.classList.add("background-read");
+      titleNum.classList.add("hide");
     });
   });
-
 });
 
 // notiDiv.addEventListener("click", retirar_classes);
